a71349332c3975655070f4005e41c0e836a290e4
fzaniniii <zaniniferreira@gmail.com>
2025-12-30 11:04:01 -0300

    Projeto Criado

---
+++ index.js
@@ -0,0 +1,9 @@
+let time = [
+    ["Pikachu", "Elétrico"],
+    ["Charmander", "Fogo"],
+    ["Bublasaur", "Planta"]
+]
+
+for (let i = 0 ; i < time.length; i++) {
+    console.log("Nome: " + time[i][0] + "| Tipo: " + time[i][1])
+}
