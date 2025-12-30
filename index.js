5088a383ba428636d522baf7943d421dfb34db62
fzaniniii <zaniniferreira@gmail.com>
2025-12-30 11:14:20 -0300

    Meu segundo commit

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-let time = [
-    ["Pikachu", "Elétrico"],
-    ["Charmander", "Fogo"],
-    ["Bublasaur", "Planta"]
-]
-
-for (let i = 0 ; i < time.length; i++) {
-    console.log("Nome: " + time[i][0] + "| Tipo: " + time[i][1])
-}
+adfasdf
+console.log("OI")
